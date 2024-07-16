5e58400985c3d263181ac897a369f0a9a5cf5de9
green-games <mgreen72194@gmail.com>
2024-07-15 23:45:27 -0500

    BATMAN

---
+++ src/main.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const Game = @import("engine").game.Game;
+const World = @import("engine").world;
+const Entity = @import("engine").entity.Entity;
+
+pub fn main() !void {
+    var game: Game = Game{};
+    game.setUp().run();
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
